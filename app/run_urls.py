43a607d7fae7ac0d04e8fcaf987c033a89855a91
Vincent Cantin Bellemare <vincent@yapigo.com>
2024-04-25 19:34:12 -0400

    updates

--- app/run_urls.py
+++ app/run_urls.py
@@ -49,7 +49,7 @@ class WebScraper:
             'response_increment': fetch_data['increment'],
             'response_status': 'true' if fetch_data['status'] else 'false',
             'response_screenshot_b64': fetch_data['screenshot_b64'] if fetch_data['screenshot_b64'] else '',
-            'response_content_b64': fetch_data['content_gzip_b64'] if fetch_data['content_gzip_b64'] else '',
+            'response_content_gzip_b64': fetch_data['content_gzip_b64'] if fetch_data['content_gzip_b64'] else '',
         }
 
         try:
