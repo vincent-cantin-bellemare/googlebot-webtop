8755acb17fe7d7c972464f1c69c284a9f7eb69db
Vincent Cantin Bellemare <vincent@yapigo.com>
2024-04-28 14:25:42 -0400

    updates

--- README.md
+++ README.md
@@ -21,6 +21,13 @@ python3 run.py --execresults --fromindex 1 --toindex 11
 python3 run.py --execurls --fromindex 1 --toindex 11
 ```
 
+## Tor
+
+```sh
+mkdir tor/ && cd tor/
+wget -q -O ./tor-browser-linux64.tar.xz "https://www.torproject.org/dist/torbrowser/13.0.14/tor-browser-linux-x86_64-13.0.14.tar.xz"
+```
+
 Ubuntu Installation
 
 ```sh
--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import subprocess
 
 
 class Runner:
-    PROJECT_NAME = "googlebotwebtop"
+    PROJECT_NAME = "googlebot-webtop"
 
     def log(self, message):
         """ Logs a message """
@@ -108,7 +108,7 @@ if __name__ == '__main__':
 
         for client_id in range(args.fromindex, args.toindex + 1):
             runner.log(f'Starting app {client_id}/{args.toindex}...')
-            thread = threading.Thread(target=runner.run_docker_app_abc, args=(client_id, ['bash', '-c', "cd /app && python3 run_results.py"]))
+            thread = threading.Thread(target=runner.run_docker_app_abc, args=(client_id, ['bash', '-c', "cd /app && /venv/bin/python3.11 run_results.py"]))
             thread.start()
             runner.sleep(5)
     elif args.execurls:
@@ -116,7 +116,7 @@ if __name__ == '__main__':
 
         for client_id in range(args.fromindex, args.toindex + 1):
             runner.log(f'Starting app {client_id}/{args.toindex}...')
-            thread = threading.Thread(target=runner.run_docker_app_abc, args=(client_id, ['bash', '-c', "cd /app && python3 run_urls.py"]))
+            thread = threading.Thread(target=runner.run_docker_app_abc, args=(client_id, ['bash', '-c', "cd /app && /venv/bin/python3.11 run_urls.py"]))
             thread.start()
             runner.sleep(5)
     else:
