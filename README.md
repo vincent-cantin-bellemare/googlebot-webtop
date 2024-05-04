82afeaa1eb09f15b226ba4af6f68221aa920fbbf
Vincent Cantin Bellemare <vincent@yapigo.com>
2024-05-03 21:07:20 -0400

    updates

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 ```sh
 mkdir tor/ && cd tor/
 wget -q -O ./tor-browser-linux64.tar.xz "https://www.torproject.org/dist/torbrowser/13.0.14/tor-browser-linux-x86_64-13.0.14.tar.xz"
-sudo docker build -t googlebot-client:1.0.0 .
+docker build -t googlebot-client:1.0.0 .
+docker compose up -d
 ```
 
 ### Scripts run
--- app/utils.py
+++ app/utils.py
@@ -60,7 +60,8 @@ def fetch_url(tor_client, url, fetch_max_increment=1):
             status_ok = \
                 html.find('Nos systèmes ont détecté un') == -1 and \
                 html.find('Ce réseau est bloqué') == -1 and \
-                html.find('Our systems have detected') == -1
+                html.find('Our systems have detected') == -1 and \
+                html.find('Before you continue to Google') == -1
 
             screenshot_b64 = compress_and_convert_screenshot_to_base64(tor_client)
             content_b64 = get_response_content_base64(tor_client)
